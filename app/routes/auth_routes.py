e3d5fde78bb18fd9b9837b3bf60af87768c3b9b6
hatikuay <ercan.erkalkan@marmara.edu.tr>
2024-11-05 19:55:04 +0300

    6. hafta

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,6 +1,16 @@
-from flask import Blueprint, request, jsonify, render_template, flash, redirect, url_for
-from app import db
+from flask import (
+    Blueprint,
+    request,
+    jsonify,
+    render_template,
+    flash,
+    redirect,
+    session,
+    url_for,
+)
+from app import db, bcrypt
 from app.models import User
+from werkzeug.security import check_password_hash
 
 auth_routes = Blueprint("auth_routes", __name__)
 
@@ -9,7 +19,24 @@ auth_routes = Blueprint("auth_routes", __name__)
 @auth_routes.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        pass
+        user_name = request.form["username"]
+        password = request.form["password"]
+
+        if not user_name or not password:
+            flash("Lütfen tüm alanları doldurun", "danger")
+            return redirect(url_for("auth_routes.login"))
+
+        user = User.query.filter_by(user_name=user_name).first()
+
+        if user and bcrypt.check_password_hash(user.password_hash, password):
+            session["user_id"] = user.id
+            flash("Başarıyla giriş yaptınız!", "success")
+            #return redirect(url_for("auth_routes.login"))
+            return render_template("dashboard.html")
+
+        flash("Geçersiz kullanıcı adı veya şifre", "danger")
+        return redirect(url_for("auth_routes.login"))
+
     return render_template("login.html")
 
 
@@ -25,11 +52,11 @@ def register():
             flash("Şifreler eşleşmiyor", "danger")
 
         if User.query.filter_by(user_name=username).first():
-            flash('Kullanıcı adı zaten alınmış!', 'danger')
-            return redirect(url_for('auth_routes.register'))
+            flash("Kullanıcı adı zaten alınmış!", "danger")
+            return redirect(url_for("auth_routes.register"))
         if User.query.filter_by(email=email).first():
-            flash('Bu e-posta adresiyle zaten bir hesap mevcut!', 'danger')
-            return redirect(url_for('auth_routes.register'))
+            flash("Bu e-posta adresiyle zaten bir hesap mevcut!", "danger")
+            return redirect(url_for("auth_routes.register"))
 
         new_user = User(user_name=username, email=email)
         new_user.set_password(password)  # Şifreyi hash'leyip kaydediyoruz
@@ -37,7 +64,14 @@ def register():
         db.session.add(new_user)
         db.session.commit()
 
-        flash('Başarıyla kayıt oldunuz!', 'success')
-        return redirect(url_for('auth_routes.login'))
+        flash("Başarıyla kayıt oldunuz!", "success")
+        return redirect(url_for("auth_routes.login"))
 
     return render_template("register.html")
+
+
+@auth_routes.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    flash("Başarıyla çıkış yaptınız!", "success")
+    return redirect(url_for("auth_routes.login"))
