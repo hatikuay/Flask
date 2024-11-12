e4a9295d04a9b5254d62a2d13091c5bce50c7b42
hatikuay <ercan.erkalkan@marmara.edu.tr>
2024-11-12 20:05:26 +0300

    7.hafta 2.commit

--- app/models.py
+++ app/models.py
@@ -10,19 +10,65 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(150), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
+    notifications = db.relationship("Notification", backref="user", lazy=True)
 
     def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        self.password_hash = bcrypt.generate_password_hash(
+            password).decode("utf-8")
 
     def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password=password)  
-    class Account(db.Model):
+        return bcrypt.check_password_hash(self.password_hash, password=password)
+
+
+class Account(db.Model):
     __tablename__ = "accounts"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    account_type = db.Column(db.String(50), unique=True, nullable=False)
+    account_type = db.Column(db.String(50), nullable=False)
     balance = db.Column(db.Float, nullable=False, default=0.0)
     interest_rate = db.Column(db.Float, nullable=False)
     maturity_date = db.Column(db.DateTime, nullable=True)
     last_deposit = db.Column(db.Float, nullable=True)
     last_withdrawal = db.Column(db.Float, nullable=True)
+
+
+class CreditCard(db.Model):
+    __tablename__ = "credit_cards"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    credit_limit = db.Column(db.Float, nullable=False)
+    current_dept = db.Column(db.Float, nullable=False, default=0.0)
+
+
+class Transaction(db.Model):
+    __tablename__ = "transactions"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    transaction_type = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+
+
+class IncomeExpense(db.Model):
+    __tablename__ = "income_expenses"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+
+
+class Notification(db.Model):
+    __tablename__ = "notifications"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    message = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+    is_read = db.Column(db.Boolean, default=False)
+
+
+class QuickAction(db.Model):
+    __tablename__ = "notifications"
+    id = db.Column(db.Integer, primary_key=True)
+    action_name = db.Column(db.String(100), nullable=False)
+    action_url = db.Column(db.String(250), nullable=False)
