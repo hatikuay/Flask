64eaae1bd630620e40647dcd3639acc4c62b9d61
hatikuay <ercan.erkalkan@marmara.edu.tr>
2024-11-12 19:53:41 +0300

    7. hafta

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from datetime import datetime, timezone
 # from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, bcrypt
 
-
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -16,4 +15,13 @@ class User(db.Model):
         self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
 
     def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password=password)
+        return bcrypt.check_password_hash(self.password_hash, password=password)  class Account(db.Model):
+    __tablename__ = "accounts"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    account_type = db.Column(db.String(50), unique=True, nullable=False)
+    balance = db.Column(db.Float, nullable=False, default=0.0)
+    interest_rate = db.Column(db.Float, nullable=False)
+    maturity_date = db.Column(db.DateTime, nullable=True)
+    last_deposit = db.Column(db.Float, nullable=True)
+    last_withdrawal = db.Column(db.Float, nullable=True)
